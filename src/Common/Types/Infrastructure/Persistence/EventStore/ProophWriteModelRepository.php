167dbd6be2d8ba8cf404cd9f7d3bca396ec78ff1
oscarlopez1616 <oscar.lopez.labrador@gmail.com>
2020-01-10 23:03:10 +0100

    orm stuff

--- src/Common/Types/Infrastructure/Persistence/EventStore/ProophWriteModelRepository.php
+++ src/Common/Types/Infrastructure/Persistence/EventStore/ProophWriteModelRepository.php
@@ -8,30 +8,17 @@ use Prooph\EventSourcing\Aggregate\AggregateRepository;
 use Prooph\EventSourcing\Aggregate\AggregateType;
 use Prooph\EventSourcing\AggregateChanged;
 use Prooph\EventSourcing\EventStoreIntegration\AggregateRootDecorator;
-use TheCodeFighters\Bundle\AuditorFramework\Common\Module\EventStore\Domain\EventStoreRepository;
 use TheCodeFighters\Bundle\AuditorFramework\Common\Types\Domain\AggregateRoot;
+use TheCodeFighters\Bundle\AuditorFramework\Common\Types\Domain\Event\Event;
 use TheCodeFighters\Bundle\AuditorFramework\Common\Types\Domain\Id;
+use TheCodeFighters\Bundle\AuditorFramework\Common\Types\Domain\Uuid;
 use TheCodeFighters\Bundle\AuditorFramework\Common\Types\Domain\WriteModelRepository;
 use TheCodeFighters\Bundle\AuditorFramework\Common\Types\Infrastructure\Exception\AggregateRootDuplicateInEventStoreException;
+use TheCodeFighters\Bundle\AuditorFramework\Common\Types\Infrastructure\Exception\AggregateRootNotFoundInEventStoreException;
 use function Lambdish\Phunctional\map;
 
-class ProophWriteModelRepository implements WriteModelRepository
+class ProophWriteModelRepository extends AggregateRepository implements WriteModelRepository
 {
-    /**
-     * @var EventStoreRepository
-     */
-    private $eventStoreRepository;
-
-    /**
-     * ProophWriteModelRepository constructor.
-     * @param EventStoreRepository $eventStoreRepository
-     */
-    public function __construct(EventStoreRepository $eventStoreRepository)
-    {
-        $this->eventStoreRepository = $eventStoreRepository;
-    }
-
-
     /**
      * @var AggregateRoot[] $aggregateRoots
      */
@@ -40,7 +27,8 @@ class ProophWriteModelRepository implements WriteModelRepository
         try {
             map(
                 function (AggregateRoot $aggregateRoot): void {
-                    $this->eventStoreRepository->save($aggregateRoot);
+                    $this->aggregateType = AggregateType::fromAggregateRoot($aggregateRoot);
+                    $this->saveAggregateRoot($aggregateRoot);
                     $aggregateRoot->flushUnPersistedRecordedEvents();
                 },
                 $aggregateRoots
@@ -53,13 +41,34 @@ class ProophWriteModelRepository implements WriteModelRepository
 
     public function findAggregateByAggregateId(Id $id, string $aggregateRootClass): AggregateRoot
     {
-        // TODO
+        $this->aggregateType = AggregateType::fromAggregateRootClass($aggregateRootClass);
+        $aggregateRoot = $this->getAggregateRoot('12836da8-0c46-491b-a1b2-85188f0c8b7a');
+        if(is_null($aggregateRoot)){
+            throw new AggregateRootNotFoundInEventStoreException($id);
+        }
+        return $aggregateRoot;
     }
 
+    /**
+     * @param Id $id
+     * @param string $eventName
+     * @return Event[]
+     */
     public function findEventsByAggregateIdAndEventName(Id $id, string $eventName): array
     {
-        // TODO: Implement findEventsByAggregateIdAndEventName() method.
+        try {
+            /** @var AggregateRoot $aggregateRoot */
+            $aggregateRoot = $this->getAggregateRoot($id->value());
+            /** @var AggregateRootDecorator $aggregateRootDecorator */
+            $aggregateRootDecorator = $this->aggregateTranslator->getAggregateRootDecorator();
+            /** @var AggregateChanged[] $recordedEvents */
+            $recordedEvents = $aggregateRootDecorator->extractRecordedEvents($aggregateRoot);
+            foreach ($recordedEvents as $recordedEvent) {
+                var_dump($recordedEvent);
+                die();
+            }
+        } catch (Exception $e) {
+            throw new AggregateRootNotFoundInEventStoreException($id);
+        }
     }
-
-
 }
